39ca99d6634d2d6ade909c5dbf7474db7be7c5e7
thiyangt <tstalagala@gmail.com>
2026-04-22 21:16:43 +0530

    updated

---
+++ data.R
@@ -0,0 +1,7 @@
+# Spatial Data Visualisation
+
+https://tmap.geocompx.org/geodata
+
+https://damariszurell.github.io/CLEWS-EDB/1_SpatialData.html
+
+https://web.stanford.edu/class/stats253/lectures.html
